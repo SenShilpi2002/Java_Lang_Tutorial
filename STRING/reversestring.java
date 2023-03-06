a10659d0abb228d5d5ebdca3ba015c5677a2503b
Shilpi <shilpisen072002@gmail.com>
2023-03-07 02:09:41 +0530

    07march

---
+++ STRING/reversestring.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class reversestring{
+    public static void main(String args[]){
+        StringBuilder sb=new StringBuilder("Ocean view");
+
+
+        for(int i=0;i<sb.length()/2;i++){
+            int front = i;
+            int back = sb.length()-1-i;    //1.> 5-1-0=4, 2.> 5-1-1=3, 3.> 5-1-2=2 
+            char frontChar=sb.charAt(front);
+            char backChar=sb.charAt(back);
+
+            sb.setCharAt(front,backChar);
+            sb.setCharAt(back,frontChar);
+
+        }
+        System.out.println(sb);
+    }
+}
